12dda70c6653a5ffb4439a2b2a500cf2a6896b87
edsoglez <edsoglez@gmail.com>
2023-11-06 15:16:27 -0600

    added protect for dup

--- JS/main.js
+++ JS/main.js
@@ -80,46 +80,51 @@ const addButton = document.getElementById("add-button")
 addButton.addEventListener("click",()=>{
     //console.log(item_id)
     //need to check if ul li text-content has element already 
+    if(orderItems.hasOwnProperty(selectedItem.value)==false || orderItems[selectedItem.value]==0){
+        let cost = itemCosts[selectedItem.value]    
 
-    let cost = itemCosts[selectedItem.value]    
-
-    selectedItem.value==""||selectedQuantity.value==""? alert("Revisa que Articulo o Cantidad no este vacio."):itemList.innerHTML += 
-    `
-    <li id="${item_id}">
-        <div class="flex-container">
-            <div class="delete-li"> 
-                <button class="remove-item-button" onClick="document.getElementById('${item_id}').remove();orderItems['${selectedItem.value}']-=${selectedQuantity.value};console.log(orderItems)">X</button>
+        selectedItem.value==""||selectedQuantity.value==""? alert("Revisa que Articulo o Cantidad no este vacio."):itemList.innerHTML += 
+        `
+        <li id="${item_id}">
+            <div class="flex-container">
+                <div class="delete-li"> 
+                    <button class="remove-item-button" onClick="document.getElementById('${item_id}').remove();orderItems['${selectedItem.value}']-=${selectedQuantity.value};console.log(orderItems)">X</button>
+                </div>
+                <div class="li-text"> 
+                    <span>${selectedItem.value}</span>
+                </div>
+                <div class="li-quant"> 
+                    <span class="quant">${selectedQuantity.value} L</span></li>
+                </div>
             </div>
-            <div class="li-text"> 
-                <span>${selectedItem.value}</span>
-            </div>
-            <div class="li-quant"> 
-                <span class="quant">${selectedQuantity.value} L</span></li>
-            </div>
-        </div>
-        
-        
-    `
+            
+            
+        `
 
-    if(selectedItem.value=="")
-    {
+        if(selectedItem.value=="")
+        {
 
-    }
-    else{
-        if(orderItems[selectedItem.value] == null){
-            orderItems[selectedItem.value] = Number(selectedQuantity.value)//+"@"+Number(cost)
-            console.log(orderItems)
         }
         else{
-            let cuant = String(orderItems[selectedItem.value]).split("@")
-            let currentQuantity = Number(cuant[0])
-            console.log(currentQuantity)
-            orderItems[selectedItem.value] = (Number(selectedQuantity.value) + currentQuantity)//+"@"+Number(cost)
-            console.log(orderItems)
+            if(orderItems[selectedItem.value] == null){
+                orderItems[selectedItem.value] = Number(selectedQuantity.value)//+"@"+Number(cost)
+                console.log(orderItems)
+            }
+            else{
+                let cuant = String(orderItems[selectedItem.value]).split("@")
+                let currentQuantity = Number(cuant[0])
+                console.log(currentQuantity)
+                orderItems[selectedItem.value] = (Number(selectedQuantity.value) + currentQuantity)//+"@"+Number(cost)
+                console.log(orderItems)
+            }
         }
+        item_id += 1;
+    }
+    else{
+        alert("Articulo ya existe en orden, si desea cambiar la cantidad, borrelo e intente de nuevo.")
     }
 
-    item_id += 1;
+    
 
 })
 
