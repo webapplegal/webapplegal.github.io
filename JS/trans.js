f83becf85bcd745a2e075aa2cf44b193e40b8d80
edsoglez <edsoglez@gmail.com>
2023-11-07 10:27:44 -0600

    added seconds in ID

--- JS/trans.js
+++ JS/trans.js
@@ -30,7 +30,8 @@ let day = date.substring(4,6);
 let year = date.substring(9,11);
 let hour = date.substring(12,14);
 let minute = date.substring(15,17)
-let orderID = year+month+day+hour+minute
+let second = date.substring(19,20)
+let orderID = year+month+day+hour+minute+second
 
 console.log(date)
 console.log(orderID)
