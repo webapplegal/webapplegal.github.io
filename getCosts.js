0d49ae7c9c763ea10817ce4710bbd41f8c8da665
edsoglez <edsoglez@gmail.com>
2023-11-17 16:04:13 -0600

    dd

--- getCosts.js
+++ getCosts.js
@@ -1,6 +1,7 @@
     import {getDatabase, set, get, update, remove, ref, child, onValue} from "https://www.gstatic.com/firebasejs/10.3.1/firebase-database.js"; 
     window.ItemDir = ref(db,'Items/');
     window.itemCosts = {}
+    console.log(itemCosts)
 
     onValue(ItemDir, (snapshot)=>{
         snapshot.forEach(
